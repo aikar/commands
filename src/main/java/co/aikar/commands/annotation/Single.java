16409e3e0b5dd23c66347c8bdfd10d2977c47ec0
Aikar <aikar@aikar.co>
2017-01-07 16:24:23 -0500

    Update copyright year

--- src/main/java/co/aikar/commands/annotation/Single.java
+++ src/main/java/co/aikar/commands/annotation/Single.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Daniel Ennis (Aikar) - MIT License
+ * Copyright (c) 2016-2017 Daniel Ennis (Aikar) - MIT License
  *
  *  Permission is hereby granted, free of charge, to any person obtaining
  *  a copy of this software and associated documentation files (the
