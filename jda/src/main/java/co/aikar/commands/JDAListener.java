d6ab3a7f0b4306e46ae834bf91a4b0f645536447
Jeremy Wood <farachan@gmail.com>
2018-01-24 16:25:19 -0500

    Removed extraneous event handling.

--- jda/src/main/java/co/aikar/commands/JDAListener.java
+++ jda/src/main/java/co/aikar/commands/JDAListener.java
@@ -2,8 +2,6 @@ package co.aikar.commands;
 
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class JDAListener extends ListenerAdapter {
@@ -13,15 +11,6 @@ public class JDAListener extends ListenerAdapter {
     JDAListener(JDACommandManager manager) {
 
         this.manager = manager;
-    }
-    @Override
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        super.onGuildMessageReceived(event);
-    }
-
-    @Override
-    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
-        super.onPrivateMessageReceived(event);
     }
 
     @Override
