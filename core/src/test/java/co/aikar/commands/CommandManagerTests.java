1c056cea4509bf0721f14af4d3ec045af1b8c4ae
Jeremy Wood <farachan@gmail.com>
2019-03-23 12:01:49 -0400

    Added basic JUnit5 testing framework. (#202)

---
+++ core/src/test/java/co/aikar/commands/CommandManagerTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2016-2019 Daniel Ennis (Aikar) - MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package co.aikar.commands;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class CommandManagerTests {
+
+    @Spy
+    private TestCommandSender sender;
+
+    private TestCommandManager manager;
+
+    @BeforeEach
+    public void initEach() {
+        manager = new TestCommandManager();
+        manager.registerCommand(new EchoCommand());
+    }
+
+    @Test
+    public void echoCommandSendsMessageArgument() {
+        manager.dispatchCommand(sender, "echo this is a test");
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(sender).sendMessage(argumentCaptor.capture());
+        assertEquals("this is a test", argumentCaptor.getValue());
+    }
+}
---
+++ core/src/test/java/co/aikar/commands/TestRootCommand.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2016-2019 Daniel Ennis (Aikar) - MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package co.aikar.commands;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRootCommand implements RootCommand {
+
+    private final String name;
+    boolean isRegistered = false;
+    private TestCommandManager manager;
+    private BaseCommand defCommand;
+    private SetMultimap<String, RegisteredCommand> subCommands = HashMultimap.create();
+    private List<BaseCommand> children = new ArrayList<>();
+
+    TestRootCommand(TestCommandManager manager, String name) {
+        this.manager = manager;
+        this.name = name;
+    }
+
+    @Override
+
+    public void addChild(BaseCommand command) {
+        if (this.defCommand == null || !command.subCommands.get(BaseCommand.DEFAULT).isEmpty()) {
+            this.defCommand = command;
+        }
+        addChildShared(this.children, this.subCommands, command);
+    }
+
+    @Override
+    public CommandManager getManager() {
+        return this.manager;
+    }
+
+    @Override
+    public SetMultimap<String, RegisteredCommand> getSubCommands() {
+        return this.subCommands;
+    }
+
+    @Override
+    public List<BaseCommand> getChildren() {
+        return this.children;
+    }
+
+    @Override
+    public String getCommandName() {
+        return this.name;
+    }
+
+    @Override
+    public BaseCommand getDefCommand() {
+        return defCommand;
+    }
+}
