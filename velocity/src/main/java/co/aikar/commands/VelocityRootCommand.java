b0792607dbffde62127e1fb1559b0723fb74f0c4
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-07-07 18:33:03 +0200

    Add velocity 3.0.0 support

--- velocity/src/main/java/co/aikar/commands/VelocityRootCommand.java
+++ velocity/src/main/java/co/aikar/commands/VelocityRootCommand.java
@@ -25,13 +25,15 @@ package co.aikar.commands;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
 import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
 
-public class VelocityRootCommand implements Command, RootCommand {
+public class VelocityRootCommand implements SimpleCommand, RootCommand {
 
     private final VelocityCommandManager manager;
     private final String name;
@@ -75,17 +77,22 @@ public class VelocityRootCommand implements Command, RootCommand {
     }
 
     @Override
-    public void execute(CommandSource source, String[] args) {
-        execute(manager.getCommandIssuer(source), getCommandName(), args);
-    }
-
-    @Override
-    public List<String> suggest(CommandSource source, String[] args) {
-        return getTabCompletions(manager.getCommandIssuer(source), getCommandName(), args);
-    }
-
-    @Override
     public BaseCommand getDefCommand() {
         return defCommand;
+    }
+
+    @Override
+    public void execute(Invocation invocation) {
+        execute(manager.getCommandIssuer(invocation.source()), getCommandName(), invocation.arguments());
+    }
+
+    @Override
+    public List<String> suggest(Invocation invocation) {
+        return getTabCompletions(manager.getCommandIssuer(invocation.source()), getCommandName(), invocation.arguments());
+    }
+
+    @Override
+    public CompletableFuture<List<String>> suggestAsync(Invocation invocation) {
+        return CompletableFuture.completedFuture(getTabCompletions(manager.getCommandIssuer(invocation.source()), getCommandName(), invocation.arguments()));
     }
 }
