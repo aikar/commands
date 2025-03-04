d55253c669d529d6d640be5da6ff3eb385490626
Intybyte <vaan.testwork@gmail.com>
2025-03-04 18:27:24 +0100

    Remove unused constructor + extra check

--- sponge10/src/main/java/co/aikar/commands/SpongeCommandSource.java
+++ sponge10/src/main/java/co/aikar/commands/SpongeCommandSource.java
@@ -1,8 +1,6 @@
 package co.aikar.commands;
 
 import org.spongepowered.api.command.CommandCause;
-import org.spongepowered.api.command.parameter.CommandContext;
-import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.util.locale.LocaleSource;
 import org.spongepowered.api.util.locale.Locales;
 
@@ -12,20 +10,12 @@ public class SpongeCommandSource {
     private final CommandCause commandCause;
     private final LocaleSource localeSource;
 
-    public SpongeCommandSource(CommandContext commandContext) {
-        commandCause = commandContext.cause();
-        Subject subject = commandContext.subject();
-        if (subject instanceof LocaleSource) {
-            localeSource = (LocaleSource) subject;
-        } else {
-            localeSource = null;
-        }
-    }
-
     public SpongeCommandSource(CommandCause cause) {
         this.commandCause = cause;
         if (cause instanceof LocaleSource) {
             localeSource = (LocaleSource) cause;
+        } else if (cause.subject() instanceof LocaleSource) {
+            localeSource = (LocaleSource) cause.subject();
         } else {
             localeSource = null;
         }
